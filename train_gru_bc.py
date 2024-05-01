8661ea15c72fd60f14ba6711825979e9e5ad6593
Darth Coder <108871750+CAI23sbP@users.noreply.github.com>
2024-05-01 17:01:53 +0900

    Update train_gru_bc.py

--- train_gru_bc.py
+++ train_gru_bc.py
@@ -12,21 +12,30 @@ from gymnasium.experimental.wrappers.rendering import RecordVideoV0
 from gymnasium.experimental.wrappers.common import RecordEpisodeStatisticsV0
 import gymnasium as gym
 import torch.nn as nn 
+import torch 
+import random
+random.seed(0)
 
-TEST_EPI = 100
+TEST_EPI = 20
 MAX_ITER = 500_000
-N_STEP = 128
+N_STEP = 256
 ENV_NAME = "BipedalWalker-v3" # "BipedalWalker-v3" , 
 BC_EPISODE = 250
+
+"""
+make env
+"""
 rng = np.random.default_rng(0)
-env = make_vec_env("BipedalWalker-v3", 
+env = make_vec_env(ENV_NAME, 
                    rng= rng, 
                    n_envs = 40, 
                    post_wrappers=[lambda env, _: RolloutInfoWrapper(env)],
                    )
 
+"""
+make expert
+"""
 policy_kwargs = dict(
-        share_features_extractor = True,
         net_arch = dict(pi = [32,32], vf = [32,32]),
         activation_fn = nn.ReLU,
         )
@@ -44,6 +53,7 @@ expert = PPO(policy='MlpPolicy',
 
 expert.learn(MAX_ITER)
 expert.save('/home/cai/Desktop/PILRnav/expert')
+
 rollouts = rollout.rollout(
     expert,
     env,
@@ -52,9 +62,25 @@ rollouts = rollout.rollout(
 )
 transitions = rollout.flatten_trajectories(rollouts)
 
+"""
+make PPO learner 
+"""
+learner = PPO(policy='MlpPolicy',
+            env = env,
+            batch_size = 64,
+            n_steps=N_STEP,
+            policy_kwargs = policy_kwargs,
+            n_epochs = 7,
+            learning_rate= 0.0004,
+            tensorboard_log= '/home/cai/Desktop/PILRnav/tensor_log/expert',
+            verbose= 1
+            )
+"""
+traininig bc with PPO learner 
+"""
 origin_bc = CustomBC(observation_space=env.observation_space,
       action_space=env.action_space,
-      policy=expert.policy,
+      policy=learner.policy,
     demonstrations = transitions,
     rng= rng,
     device='cuda',
@@ -64,40 +90,36 @@ origin_bc.train(n_epochs = 10)
 
 origin_bc.policy.save('/home/cai/Desktop/PILRnav/bc')
 
-del rollouts, transitions
-
+"""
+make GRU PPO learner 
+"""
 policy_kwargs = dict(
         share_features_extractor = True,
         net_arch = dict(pi = [32,32], vf = [32,32]),
-        gru_hidden_size = 64,
+        gru_hidden_size = 32,
         activation_fn = nn.ReLU,
         )
-expert_gru = GruPPO(policy='MlpGruPolicy',
-             env = env,
+
+learner_gru = GruPPO(policy='MlpGruPolicy',
+            env = env,
             batch_size = 64,
             n_steps=N_STEP,
             policy_kwargs = policy_kwargs,
             n_epochs = 7,
             learning_rate= 0.0004,
-            tensorboard_log= '/home/cai/Desktop/PILRnav/tensor_log/expert_gru',
+            tensorboard_log= '/home/cai/Desktop/PILRnav/tensor_log/expert',
             verbose= 1
             )
-
-expert_gru.learn(MAX_ITER)
-rollouts = rollout.rollout(
-    expert_gru,
-    env,
-    rollout.make_sample_until(min_timesteps=None, min_episodes = BC_EPISODE),
-    rng=rng,
-)
-transitions = rollout.flatten_trajectories(rollouts)
-
+"""
+training GRU bc with GRU PPO 
+"""
 recurrent_bc_ = GruBC(venv=env, 
-                   policy=expert_gru.policy,
+                   policy=learner_gru.policy,
                    demonstrations = transitions,
                    rng = rng,
                    device='cuda',
                    tensorboard_log = '/home/cai/Desktop/PILRnav/tensor_log/gru_bc')
+
 recurrent_bc_.train(n_epochs=5, n_batches = 32)
 recurrent_bc_.policy.save('/home/cai/Desktop/PILRnav/gru_bc')
 del env, rollouts, transitions
@@ -106,13 +128,14 @@ del env, rollouts, transitions
 
 epi_rewards = []
 epi_lenghts = []
-for index, agent in enumerate([expert, expert_gru, origin_bc ,recurrent_bc_]):
-    main_env = gym.make('BipedalWalker-v3', max_episode_steps= 400, render_mode = 'rgb_array')
+keys = ['expert', 'bc', 'bc_gru']
+for index, agent in enumerate([expert, origin_bc ,recurrent_bc_]):
+    main_env = gym.make(ENV_NAME, max_episode_steps= 400, render_mode = 'rgb_array')
     env_record = RecordVideoV0(env = main_env, 
                             video_folder = '/home/cai/Desktop/PILRnav/record',
                             video_length = 0,
-                            name_prefix = 'expert' if index == 0 else 'expert_gru' if index == 1 else 'bc' if index == 2 else 'gru_bc' ,
-                            episode_trigger = lambda x: x % 10 == 0
+                            name_prefix = keys[index],
+                            episode_trigger = lambda x: x % 4 == 0
                             )
     env_record = RecordEpisodeStatisticsV0(env = env_record, 
                                            )
@@ -140,7 +163,6 @@ for index, agent in enumerate([expert, expert_gru, origin_bc ,recurrent_bc_]):
     del env_record
     
 import pandas as pd 
-keys = ['expert', 'expert_gru','bc', 'bc_gru']
 epi_infos = {ke:[epi_re,epi_le] for ke, epi_re, epi_le in zip(keys,epi_rewards, epi_lenghts)}
 data_frame = pd.DataFrame(epi_infos)
 print(data_frame)
